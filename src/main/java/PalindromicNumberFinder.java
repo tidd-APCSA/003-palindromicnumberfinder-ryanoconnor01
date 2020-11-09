a9127c2b00bc4ac7045fd748a0ef93b94709dd77
Ryan O'Connor <rcoconnor5@gmail.com>
2020-11-09 15:29:51 +0000

    finished up last two methods

--- src/main/java/PalindromicNumberFinder.java
+++ src/main/java/PalindromicNumberFinder.java
@@ -13,21 +13,37 @@ public class PalindromicNumberFinder {
 
     // this method should find the next palindromic number
     public int searchForPalindromicNum(int num){
-
+      int searchNum = num;
+      for (int i = 1; i>0; i++){
+        searchNum = searchNum + i;
+        if (testPalindromicNum(searchNum) == true){
+          break;
+        }
+      }
+      return searchNum;
     }
 
     // this is a helper method for searchForPalindromicNum. It's purpose is to test if a number is actually a palindrome
     public boolean testPalindromicNum(int num){
-
+        if (number.equals(reverse)) {
+          return true;
+        }
+      else{
+        return false;
+      }
 
     }
 
 
     // this is a helper method for testPalindromicNum. It should reverse the number and return it.
     public String reverseNum(int num){
-
-
-
+      String number = Integer.toString(num);
+      //did this for wrapper class bc primitive variables do not have methods associated with them - so use wrpper class to do int to string
+      String reverse = "";
+      for (int i = number.length()-1; i>= 0; i--){
+        reverse = reverse + number.charAt(i);
+      }
+        return reverse;
     }
 
 
